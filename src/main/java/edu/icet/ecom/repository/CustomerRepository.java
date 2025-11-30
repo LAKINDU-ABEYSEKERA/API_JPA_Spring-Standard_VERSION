91adbf784dde98eef308c8ef2526d50b18dc052d
LAKINDU-ABEYSEKERA <2005nadu@gmail.com>
2025-11-30 08:21:49 +0530

    Initial Commit!..

---
+++ src/main/java/edu/icet/ecom/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package edu.icet.ecom.repository;
+
+import edu.icet.ecom.model.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
