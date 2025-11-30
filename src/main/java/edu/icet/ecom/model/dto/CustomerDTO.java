cb60b3d74efa87eca0a7b33697f049bd8f4a297e
LAKINDU-ABEYSEKERA <2005nadu@gmail.com>
2025-11-30 08:21:49 +0530

    Initial Commit!..

--- src/main/java/edu/icet/ecom/model/dto/CustomerDTO.java
+++ src/main/java/edu/icet/ecom/model/dto/CustomerDTO.java
@@ -1,17 +1,4 @@
 package edu.icet.ecom.model.dto;
 
-import lombok.*;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-
 public class CustomerDTO {
-
-    private Integer customerId;
-    private String name;
-    private String address;
-    private String contact;
 }
--- src/main/java/edu/icet/ecom/model/entity/Customer.java
+++ src/main/java/edu/icet/ecom/model/entity/Customer.java
@@ -1,20 +1,4 @@
 package edu.icet.ecom.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.*;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Setter
-@Getter
-@ToString
-
-@Entity
 public class Customer {
-    @Id
-    private int customerId;
-    private String name;
-    private String address;
-    private String contact;
 }
