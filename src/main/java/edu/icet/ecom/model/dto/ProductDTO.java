97e9d4724a15c8842e90e2feb5e159f8a31f88c8
LAKINDU-ABEYSEKERA <2005nadu@gmail.com>
2025-12-02 22:59:55 +0530

    Add Product Method Implemented!..

--- src/main/java/edu/icet/ecom/model/dto/ProductDTO.java
+++ src/main/java/edu/icet/ecom/model/dto/ProductDTO.java
@@ -1,4 +1,17 @@
 package edu.icet.ecom.model.dto;
 
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+
 public class ProductDTO {
+    private Integer productId;
+    private String name;
+    private int price;
 }
